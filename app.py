461c14bc1d81057a6628b8f230c95d42971d5aa1
rioseleng09 <94964007+rioseleng09@users.noreply.github.com>
2023-12-13 17:58:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing import image
 import pickle
@@ -15,14 +14,12 @@ def load_model():
     model = model_from_json(loaded_model_json)
 
     # Load weights into the new model
-    model.load_weights('model.h5')
+    model.load_weights("model.h5")
 
     return model
 
-# ...
-
 # Function to make a diagnosis
-def diagnosis(file, model, IMM_SIZE):
+def diagnosis(file, model):
     # Load and preprocess the image
     img = image.load_img(file, target_size=(IMM_SIZE, IMM_SIZE), color_mode="grayscale")
     img_array = image.img_to_array(img)
@@ -34,16 +31,14 @@ def diagnosis(file, model, IMM_SIZE):
     predicted_class = np.argmax(predicted_probabilities, axis=-1)[0]
 
     # Map the predicted class to the diagnosis
-    diagnosis_mapping = {0: "Covid", 1: "Normal", 2: "Pneumonia"}
+    diagnosis_mapping = {0: "Diagnosis_A", 1: "Diagnosis_B", 2: "Diagnosis_C"}
     predicted_diagnosis = diagnosis_mapping[predicted_class]
 
     return predicted_diagnosis
 
-# ...
-
 # Main Streamlit app
 def main():
-    st.title("Chest X-ray Image Diagnosis App")
+    st.title("Medical Image Diagnosis App")
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
@@ -54,19 +49,10 @@ def main():
         # Load the model
         model = load_model()
 
-        # Specify the image size
-        IMM_SIZE = 224
-
-        try:
-            # Get diagnosis
-            result = diagnosis(uploaded_file, model, IMM_SIZE)
-            st.success(f"The predicted diagnosis is: {result}")
-        except Exception as e:
-            st.error(f"Error during diagnosis: {e}")
-            print("Error during diagnosis:", e)
-
-# ...
-
+        # Get diagnosis
+        result = diagnosis(uploaded_file, model)
+        
+        st.success(f"The predicted diagnosis is: {result}")
 
 if __name__ == "__main__":
     main()
